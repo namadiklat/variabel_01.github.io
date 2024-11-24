ee07316ad16a1d8887cb0eac33a3a95d899278e4
namadiklat <namadiklat@gmail.com>
2024-11-24 12:12:22 +0700

    Add files via upload

---
+++ variabel_01.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int x; //deklarasi variabel x dengan tipe data int
+x = 15; //mengisi nilai variabel x dengan nilai 5
+cout<<x; //mencetak isi variabel x
+}
